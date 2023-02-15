1f1a3a87b2b1bed42132e412e29930f7190e334b
Lucas Zosi <zosi.lucas@gmail.com>
2023-02-15 10:20:39 -0300

    Add strict mode again

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
